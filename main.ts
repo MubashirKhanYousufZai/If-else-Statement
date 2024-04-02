7a0d06eac5a8e27343c54e1d274e442e6336bd75
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-04-02 17:31:43 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,14 @@
+// If else statement
+let isSchoolDay = "Exams";
+if (isSchoolDay) {
+    console.log("Get up on time,have healthy breakfast");
+} else {
+    console.log("Play a music");
+}
+
+let shopping = true;
+if (shopping) {
+     console.log("Set a budget");
+} else {
+    console.log("Say No To Impulse Purchases.");
+}
